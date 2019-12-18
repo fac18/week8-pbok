bbc129bd707117df7fc701e328bdff0929302159
renatajarmova <renatajarmova@gmail.com>
2019-12-18 17:10:03 +0000

    Delete mistakes that need to be corrected

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const exphbs = require('express-handlebars');
+const controllers = require('./controllers/index');
+const helpers = require('./views/helpers/index');
 
 const app = express();
 
+// set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    helpers,
+  })
+);
+
 app.disable('x-powered-by');
 app.search('port', process.env.PORT || 5000);
-//app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
-
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
     express.static(path.join(__dirname, '..', 'public'), { maxAge: '100d' })
 );
+app.use(controllers);
 
+// to be moved inside controllers folder
 app.get('/', (request, response) => {
     response.render('home');
     });
@@ -33,4 +51,4 @@ app.post('/resource', (request, response) => {
 })
 });
 
-app.listen(5000);
+module.exports = app;
