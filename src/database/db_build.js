0239313d5c16846bbe52f3378d086aa18cf560f5
renatajarmova <renatajarmova@gmail.com>
2019-12-18 14:59:59 +0000

    Set up the database and test database
    co-authored-by: Renata <renatajarmova@gmail.com>
    co-authored-by: Kin <kinnamau@gmail.com>
    co-authored-by: Bethany <bethanyioscott@hotmail.co.uk>
    co-authored-by: Judith <jyouziel20@hotmail.com>

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -1,1 +1,9 @@
-// db_build.js
+const fs = require("fs");
+const dbConnection = require("./db_connection");
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+
+const runDbBuild = cb => {
+  dbConnection.query(sql, cb);
+};
+
+module.exports = runDbBuild;
