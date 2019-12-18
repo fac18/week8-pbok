62336a68bb074f1dcf524960f2fc4c2555e4c051
kin-au <kinnamau@gmail.com>
2019-12-18 22:25:30 +0000

    fix server

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-const controllers = require('./controllers/index');
+const controllers = require('./controllers');
 const helpers = require('./views/helpers/index');
 
 const app = express();
 
-// set up view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine(
@@ -22,15 +22,18 @@ app.engine(
 );
 
 app.disable('x-powered-by');
-app.search('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
     express.static(path.join(__dirname, '..', 'public'), { maxAge: '100d' })
 );
-app.use(controllers);
 
-// to be moved inside controllers folder
+// comment this back in after fixing controllers files
+// app.use(controllers);
+
+// below to be moved inside controllers folder
 app.get('/', (request, response) => {
     response.render('home');
     });
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,18 @@
-// index.js
+
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+
+// create routes for home and library
+// create error handling routes
+
+// const fruit = require("./fruit");
+// const error = require("./error");
+
+// router.get("/fruit", fruit.get);
+// router.post("/fruit", fruit.post);
+// router.use(error.client);
+// router.use(error.server);
+
+module.exports = router;
