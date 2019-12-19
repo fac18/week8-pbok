7d7799a494bc447593b1a55a5179f14fa4b09281
kin-au <kinnamau@gmail.com>
2019-12-19 17:23:10 +0000

    filter function complete, add home button to header, create filter hbs
    co-authored-by: Renata <renatajarmova@gmail.com>

--- src/model/db_build.sql
+++ src/model/db_build.sql
@@ -14,8 +14,12 @@ INSERT INTO resources (title, language, description, link) VALUES
 ('Coolors', 'CSS', 'Colour scheme generator', 'https://coolors.co/'),
 ('Flex-box Froggy', 'CSS', 'Back to basics flex-box game', 'http://flexboxfroggy.com/'),
 ('CSS lengths', 'CSS', 'CSS dimensions tutorial',	'https://css-tricks.com/the-lengths-of-css/'),
-('Eloquent JavaScript', 'JavaScript', 'JavaScript online textbook',	'https://eloquentjavascript.net/'),
-('JS Regex', 'JavaScript', 'A beginners guide to regex', 'https://flaviocopes.com/javascript-regular-expressions/'),
-('UI Elements Glossary', 'HTML', 'An intro to user interface elements',	'https://www.usability.gov/how-to-and-tools/methods/user-interface-elements.html');
-
+('Eloquent JavaScript', 'JAVASCRIPT', 'JavaScript online textbook',	'https://eloquentjavascript.net/'),
+('JS Regex', 'JAVASCRIPT', 'A beginners guide to regex', 'https://flaviocopes.com/javascript-regular-expressions/'),
+('UI Elements Glossary', 'HTML', 'An intro to user interface elements',	'https://www.usability.gov/how-to-and-tools/methods/user-interface-elements.html'),
+('Express documentation', 'EXPRESS', 'Official Express documentation', 'https://expressjs.com/en/api.html'),
+('What is handlebars', 'HANDLEBARS', 'Introduction to handlebars', 'https://handlebarsjs.com/guide/#what-is-handlebars'),
+('PostgreSQL tutorial', 'SQL', 'Learn PostgreSQL from scratch', 'http://www.postgresqltutorial.com/'),
+('Node docs', 'NODE', 'Node documentation', 'https://nodejs.org/docs/latest-v11.x/api/'),
+('React tutorial', 'REACT', 'Intro to React tutorial', 'https://reactjs.org/tutorial/tutorial.html');
 COMMIT;
