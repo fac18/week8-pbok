342311d2dd42b2a9d3fb59fe0db2b4f69c0e5691
Bethany Scott <bethanyioscott@hotmail.co.uk>
2019-12-19 14:45:25 +0000

    Send data and redirect to home page

--- src/model/queries/index.js
+++ src/model/queries/index.js
@@ -18,16 +18,17 @@ const filterByLanguage = (language, cb) => {
 }
 
 const addResource = (title, language, description, link, cb) => {
+  console.log("i'm in add resource!");
   db.query(`INSERT INTO resources(title, language, description, link) VALUES ($1, $2, $3, $4)`,
   [title, language, description, link],
   (err, result) => {
     if (err) return cb(err);
-    cb(null, result.rows);
+    cb(null, result);
   });
 }
 
 module.exports = {
   getAll,
   filterByLanguage,
-  addResource,
+  addResource
 };
